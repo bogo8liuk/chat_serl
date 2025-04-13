36305ca7b13c37889966004b2f3d04d5a84fe7f4
bogo8liuk <lucaborghi99@gmail.com>
2025-04-13 10:09:07 +0200

    feat: accept multiple tcp requests

--- src/tcp.erl
+++ src/tcp.erl
@@ -6,11 +6,11 @@ listen(Port) ->
     gen_tcp:listen(Port, [binary, {packet, 0}, {active, false}]).
 
 accept(ListenSocket) ->
-    gen_tcp:accept(ListenSocket).
-        %{ok,S} ->
-        %    io:format("ALL OK!"),
-        %    accept(ListenSocket);
-        %Other ->
-        %    io:format("accept returned ~w - goodbye!~n",[Other]),
-        %    ok
-    
+    case gen_tcp:accept(ListenSocket) of
+        {ok, Socket} ->
+            logger:info("accepted tcp request"),
+            accept(ListenSocket);
+        Other ->
+            logger:info("accept returned ~w - goodbye!~n", [Other]),
+            ok
+    end.
